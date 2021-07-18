9c248d19fdba3d195bf2b98c69586ca786b6493c
adarshjhaa100 <adarshjhaa100@gmail.com>
2021-07-18 17:56:36 +0530

    first commit

---
+++ src/main/java/com/tcsc/budgetAndExpense/BudgetExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tcsc.budgetAndExpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetExpenseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcsc/budgetAndExpense/BudgetExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcsc.budgetAndExpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
