5a0d5d4047b505355db44c20c3fb5fd1c7a2bc3d
adarshjhaa100 <adarshjhaa100@gmail.com>
2022-06-12 23:41:54 +0530

    Add entity product

---
+++ src/main/java/com/tcsc/budgetAndExpense/entity/Product.java
@@ -0,0 +1,59 @@
+package com.tcsc.budgetAndExpense.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "name",length = 100, nullable = false)
+    private String name;
+    @Column(name = "desc",length = 255)
+    private String desc;
+    @Column(name="user_id")
+    private int userId;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, String desc, int userId) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
